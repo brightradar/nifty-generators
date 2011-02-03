5a5e57ff951c79d4fea97f3a190844a0e8d81ed3
Justin Schier <brightradar@gmail.com>
2011-02-02 19:34:15 -0800

    WIP: Subclassing Scaffold Generator

---
+++ lib/generators/nifty/ajax_scaffold/ajax_scaffold_generator.rb
@@ -0,0 +1,18 @@
+require 'generators/nifty/scaffold/scaffold_generator'
+
+module Nifty
+  module Generators
+    class AjaxScaffoldGenerator < ScaffoldGenerator
+		
+		# railties/lib/rails/generators.rb
+		def self.templates_path
+		  raise '8'
+		end
+		
+		
+		def self.source_root
+		  raise '14'
+		end
+    end
+  end
+end
